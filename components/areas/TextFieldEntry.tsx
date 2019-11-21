d165150bebbbf38e18647a73ad7c395a25b14287
Hendy Irawan <ceefour666@gmail.com>
2019-11-21 20:59:17 +0700

    update package.json

--- components/areas/TextFieldEntry.tsx
+++ components/areas/TextFieldEntry.tsx
@@ -7,14 +7,15 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const TextFieldEntry: FunctionComponent<{ 
-  label: string, value?: string, suffix?: string, missingText?: string,
-  [prop: string]: any }> = ({ label, value, suffix, missingText, ...props }) => {
+  label: string, value?: string, prefix?: string, suffix?: string, missingText?: string,
+  [prop: string]: any }> = ({ label, value, prefix, suffix, missingText, ...props }) => {
   const classes = useStyles();
   const defaultValue = value != undefined ? value : (missingText || " ");
   return (
     <TextField label={label} defaultValue={defaultValue}
       InputProps={{
         readOnly: true,
+        startAdornment: prefix ? <InputAdornment position="start">{prefix}</InputAdornment> : undefined,
         endAdornment: suffix ? <InputAdornment position="end">{suffix}</InputAdornment> : undefined,
       }} margin="normal" {...props} />
   );
--- pages/tsunami-sources/[tsunamiSourceId].tsx
+++ pages/tsunami-sources/[tsunamiSourceId].tsx
@@ -128,6 +128,111 @@ function TsunamiSourceId() {
           <GeoPointEntry
             label="Tsunami source location"
             featureName={tsunamiSource_panel.data.tsunamiSource.locationName}             mapboxApiAccessToken={appConfig.MAPBOX_API_ACCESS_TOKEN}             featureLat={tsunamiSource_panel.data.tsunamiSource.latitude}             featureLon={tsunamiSource_panel.data.tsunamiSource.longitude} />
+          <TextFieldEntry
+            label="Cause"
+            value={tsunamiSource_panel.data.tsunamiSource.causeCode} />
+          <TextFieldEntry
+            label="Country"
+            value={tsunamiSource_panel.data.tsunamiSource.country} />
+          <TextFieldEntry
+            label="Damage"
+            prefix="USD"
+            suffix="M"
+            value={tsunamiSource_panel.data.tsunamiSource.damageMillionsDollars} />
+          <TextFieldEntry
+            label="Damage description"
+            value={tsunamiSource_panel.data.tsunamiSource.damageDescription} />
+          <TextFieldEntry
+            label="Source event time"
+            value={((it: any) => (`${it.year}-${it.month}-${it.day} ${it.hour}:${it.minute}:${it.second} UTC`))(tsunamiSource_panel.data.tsunamiSource)} />
+          <TextFieldEntry
+            label="Magnitude"
+            value={tsunamiSource_panel.data.tsunamiSource.primaryMagnitude} />
+          <TextFieldEntry
+            label="Focal depth"
+            suffix="km"
+            value={tsunamiSource_panel.data.tsunamiSource.focalDepth} />
+          <TextFieldEntry
+            label="Max. water height"
+            suffix="m"
+            value={tsunamiSource_panel.data.tsunamiSource.maxWaterHeight} />
+          <TextFieldEntry
+            label="Warning status"
+            value={tsunamiSource_panel.data.tsunamiSource.warningStatus} />
+          <TextFieldEntry
+            label="Region code"
+            value={tsunamiSource_panel.data.tsunamiSource.regionCode} />
+          <TextFieldEntry
+            label="State"
+            value={tsunamiSource_panel.data.tsunamiSource.state} />
+          <TextFieldEntry
+            label="Event validity"
+            value={tsunamiSource_panel.data.tsunamiSource.eventValidity} />
+          <TextFieldEntry
+            label="Tsunami intensity (Soloviev)"
+            value={tsunamiSource_panel.data.tsunamiSource.tsunamiIntensitySoloviev} />
+          <TextFieldEntry
+            label="Tsunami magnitude (Abe)"
+            value={tsunamiSource_panel.data.tsunamiSource.tsunamiMagnitudeAbe} />
+          <TextFieldEntry
+            label="Tsunami magnitude (Iida)"
+            value={tsunamiSource_panel.data.tsunamiSource.tsunamiMagnitudeIida} />
+          <TextFieldEntry
+            label="Deaths from tsunami"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.deaths} />
+          <TextFieldEntry
+            label="Deaths from tsunami (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.deathsDescription} />
+          <TextFieldEntry
+            label="Injuries from tsunami"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.injuries} />
+          <TextFieldEntry
+            label="Injuries from tsunami (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.injuriesDescription} />
+          <TextFieldEntry
+            label="People missing due to tsunami"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.missing} />
+          <TextFieldEntry
+            label="People missing due to tsunami (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.missingDescription} />
+          <TextFieldEntry
+            label="Houses destroyed"
+            suffix="houses"
+            value={tsunamiSource_panel.data.tsunamiSource.housesDestroyed} />
+          <TextFieldEntry
+            label="Houses destroyed by tsunami (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.housesDestroyedDescription} />
+          <TextFieldEntry
+            label="Houses damaged by tsunami"
+            suffix="houses"
+            value={tsunamiSource_panel.data.tsunamiSource.housesDamaged} />
+          <TextFieldEntry
+            label="Houses damaged by tsunami (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.housesDamagedDescription} />
+          <TextFieldEntry
+            label="Total deaths"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.totalDeaths} />
+          <TextFieldEntry
+            label="Total deaths (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.totalDeathsDescription} />
+          <TextFieldEntry
+            label="Total injuries"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.totalInjuries} />
+          <TextFieldEntry
+            label="Total injuries (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.totalInjuriesDescription} />
+          <TextFieldEntry
+            label="Total missing"
+            suffix="people"
+            value={tsunamiSource_panel.data.tsunamiSource.totalMissing} />
+          <TextFieldEntry
+            label="Total missing (description)"
+            value={tsunamiSource_panel.data.tsunamiSource.totalMissingDescription} />
         </Panel>
         }
 
